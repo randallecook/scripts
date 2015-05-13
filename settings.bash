fdd9c70871a9753dacd312d7eac93fa216f82502
Randall Cook <randallecook@gmail.com>
2015-05-13 20:08:13 +0000

    added settings.bash

---
+++ settings.bash
@@ -0,0 +1,14 @@
+# Here are some bash settings that I find useful:
+
+# command prompt
+PS1='\u@\h#\#:\w$ '
+
+# directory listing
+alias l='ls -oh'
+
+# find in files
+alias fif='ack-grep'
+
+# count git commits
+alias cgc='git rev-list --all | wc -l'
+
